e3f4393c017a662ea4901b4b72438456be8495a2
nach0aguilar <nachoaguilarfdz@gmail.com>
2024-05-21 21:09:44 +0200

    Added some functions and libraries, they are not tested yet

--- Makefile
+++ Makefile
@@ -6,31 +6,45 @@
 #    By: igaguila <igaguila@student.42malaga.com    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2024/05/20 16:55:12 by igaguila          #+#    #+#              #
-#    Updated: 2024/05/20 18:51:13 by igaguila         ###   ########.fr        #
+#    Updated: 2024/05/21 14:25:06 by igaguila         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 NAME_C = client
 NAME_S = server
 
-SRC = server.c client.c
+SRC_C = src/client.c
+SRC_S = src/server.c
+
+OBJ_C = $(SRC_C:.c=.o)
+OBJ_S = $(SRC_S:.c=.o)
 
 OBJ = $(SRC:.c=.o)
 
 CC = gcc
 CFLAGS = -Wall -Wextra -Werror
 
-all: $(NAME)
+LIBFT_DIR = includes/libft
+LIBFT = ${LIBFT_DIR}/libft.a
 
-$(NAME): $(OBJ)
-	@$(CC) $(CFLAGS) server.o -o server
-	@$(CC) $(CFLAGS) client.o -o client
+PRINTF_DIR = includes/ft_printf
+PRINTF = ${PRINTF_DIR}/ft_printf.a
+
+all: $(OBJ_S) $(OBJ_C)
+	@make -C includes/libft
+	@make -C includes/ft_printf
+	@$(CC) $(CFLAGS) $(LIBFT) $(PRINTF) $(SRC_S) -o $(NAME_S)
+	@$(CC) $(CFLAGS) $(LIBFT) $(SRC_C) -o $(NAME_C)
 
 clean:
-	@rm -f $(OBJ)
+	@make -C ${LIBFT_DIR} clean
+	@make -C ${PRINTF_DIR} clean
+	@rm -f $(OBJ_S) $(OBJ_C)
 
 fclean: clean
-	@rm -f server client
+	@make -C ${LIBFT_DIR} fclean
+	@make -C ${PRINTF_DIR} fclean
+	@rm -f $(NAME_S) $(NAME_C)
 
 re: fclean all
 
--- src/server.c
+++ src/server.c
@@ -6,7 +6,61 @@
 /*   By: igaguila <igaguila@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 16:55:31 by igaguila          #+#    #+#             */
-/*   Updated: 2024/05/20 16:55:32 by igaguila         ###   ########.fr       */
+/*   Updated: 2024/05/21 13:31:13 by igaguila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/libft/libft.h"
+#include "../includes/ft_printf/ft_printf.h"
+#include <signal.h>
+#include <unistd.h>
+
+void text_converter(char *c)
+{
+    int i;
+    int base;
+    char letter;
+
+    i = 0;
+    while (i < 8)
+    {
+        base = 128;
+        letter = 0;
+        while (base > 0)
+        {
+            if (c[i] == '1')
+                letter += base;
+            base /= 2;
+            i++;
+        }
+        ft_printf("%c", letter);
+    }
+}
+
+void handle_signal(int signal)
+{
+    static int i = 0;
+    static char c[8];
+
+    if (signal == SIGUSR1)
+        c[i] = '1';
+    else if (signal == SIGUSR2)
+        c[i] = '0';
+    i++;
+    if (i == 8)
+    {
+        i = 0;
+        text_converter(c);
+    }
+}
+
+int main()
+{
+    getpid();
+    ft_printf("%d\n", getpid());
+    signal(SIGUSR1, handle_signal);
+    signal(SIGUSR2, handle_signal);
+    while (1)
+        sleep(1);
+    return (0);
+}
